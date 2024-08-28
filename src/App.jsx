7c60553db803ccc23e0b2ed66de157f386268f62
Alexander12302002 <106930173+Alexander12302002@users.noreply.github.com>
2024-08-27 20:27:55 -0500

    feat: :tada: inicio del portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,24 @@ function App() {
 
   return (
     <>
+      <div className="container-main">
+        <nav className="horizontal-bar">
+          <img></img>
+          <div className='items'>
+            <ul>
+              <li>Inicio</li>
+              <li>Acerca de mi</li>
+              <li>Proyectos</li>
+            </ul>
+          </div>
+          <div className='Contact-me'>
+            <button><a href='#'>Contactame</a></button>
+          </div>
+        </nav>
+        <div className="container">
+          
+        </div>
+      </div>
     </>
   )
 }
